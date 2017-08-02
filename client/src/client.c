3e0c36054f848bd28903404528538c1a93bb53bb
Dmitry Medvedev <demon0900@mail.ru>
2017-08-02 16:36:32 +0600

    sending keys to server

--- client/src/client.c
+++ client/src/client.c
@@ -1,11 +1,13 @@
 #include "../include/core.h"
 
 int main(int argc, char **argv) {
-    int sd = 0, i = 0, j = 0;
-    unsigned char p_id, dst_ip[IP_SIZE], src_ip[IP_SIZE];
-    unsigned short int dst_port = 0, src_port = 0;
+    int sd = 0, i = 0, j = 0, *status = NULL;
+    uint8_t p_id, dst_ip[IP_SIZE], src_ip[IP_SIZE];
+    uint16_t dst_port = 0, src_port = 0;
+    pthread_t ctl_hndl_tid;
     struct sockaddr_in dst_addr;
     struct hostent *dst_hst = NULL;
+    connect_info_t c_info;
 
     parse_argvs(argc, argv, dst_ip, &dst_port, src_ip, &src_port);
     printf("[ARGS] DST: %s:%hu\n       SRC: %s:%hu\n", dst_ip, dst_port, src_ip, src_port);
@@ -17,10 +19,13 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    c_info.sd = sd;
+    c_info.dst_addr = dst_addr;
+
     printf("[INFO] Player ID: %hu\n", p_id);
 
     dst_hst = gethostbyaddr((char *)&dst_addr.sin_addr.s_addr, 4, AF_INET);
-    printf("Server: %s:%hu (hs: %s)\n", inet_ntoa(dst_addr.sin_addr), ntohs(dst_addr.sin_port),
+    printf("Connected to the server: %s:%hu (host name: %s)\n", inet_ntoa(dst_addr.sin_addr), ntohs(dst_addr.sin_port),
     ((dst_hst != NULL) ? dst_hst->h_name : ""));
 
     printf("\n");
@@ -30,6 +35,22 @@ int main(int argc, char **argv) {
 
         printf("\n");
     }
+    printf("\n");
+
+/*1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111*/
+        if (initscr() == NULL) {
+                perror("initscr");
+                exit(EXIT_FAILURE);
+        }
+
+        noecho();
+        curs_set(0);
+        keypad(stdscr, true);
+        cbreak();
+/*1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111*/
+
+    pthread_create(&ctl_hndl_tid, NULL, control_hndl, (void *)&c_info);
+    pthread_join(ctl_hndl_tid, (void **)status);
 
     close(sd);
     exit(EXIT_SUCCESS);
