50c1b205d186e09cadb9db44ff76584ad1b44da2
Dmitry Medvedev <demon0900@mail.ru>
2017-08-02 17:00:20 +0600

    merge dev-gui

--- client/lib/gui.c
+++ client/lib/gui.c
@@ -1,28 +1,36 @@
 #include "../include/gui.h"
-unsigned char map[MAP_H][MAP_W];
 
-extern NCURSES_EXPORT(int) waddnwstr (WINDOW *,const wchar_t *,int);
-extern NCURSES_EXPORT(int) waddwstr (WINDOW *,const wchar_t *);
+extern NCURSES_EXPORT(int) waddnwstr (WINDOW *, const wchar_t *,int);
+extern NCURSES_EXPORT(int) waddwstr (WINDOW *, const wchar_t *);
 
 void init_graph() {
     WINDOW *rtn = NULL;
 
     setlocale(LC_ALL, "");
+
     rtn = initscr();
     if (rtn == NULL) {
         perror("Client: initscr error");
         exit(EXIT_FAILURE);
     }
+
     noecho();
     curs_set(0);
     keypad(stdscr, true);
     cbreak();
     start_color();
     refresh();
+
     /*LOG - init_graph*/
 }
 
 void init_surf(surface_t **surface) {
+    *surface = calloc(1, sizeof(surface_t));
+    if (*surface == NULL) {
+        perror("Client: calloc(surface)");
+        exit(EXIT_FAILURE);
+    }
+
     (*surface)->p_wnd_map = newwin(MAP_H, MAP_W, INFO_H + 1, 1);
     box((*surface)->p_wnd_map, 0, 0);
     (*surface)->wnd_map = derwin((*surface)->p_wnd_map,
@@ -43,11 +51,13 @@ void del_surf(surface_t **surface){
     delwin((*surface)->p_wnd_info);
     delwin((*surface)->wnd_map);
     delwin((*surface)->wnd_info);
+
     endwin();
 }
 
 void print_map(surface_t *surface, unsigned char p_id){
     int i = 0, j = 0;
+
     for (i = 0; i < MAP_H; ++i) {
         for(j = 0; j < MAP_W; ++j) {
             switch (map[i][j]) {
@@ -74,5 +84,6 @@ void print_map(surface_t *surface, unsigned char p_id){
             }
         }
     }
+
     wrefresh(surface->wnd_map);
 }
--- client/src/client.c
+++ client/src/client.c
@@ -1,16 +1,20 @@
 #include "../include/core.h"
 
 int main(int argc, char **argv) {
-    int sd = 0, i = 0, j = 0, *status = NULL;
+    int sd = 0, *status = NULL;
     uint8_t p_id, dst_ip[IP_SIZE], src_ip[IP_SIZE];
     uint16_t dst_port = 0, src_port = 0;
     pthread_t ctl_hndl_tid;
     struct sockaddr_in dst_addr;
     struct hostent *dst_hst = NULL;
     connect_info_t c_info;
+    surface_t *surface = NULL;
 
     parse_argvs(argc, argv, dst_ip, &dst_port, src_ip, &src_port);
     printf("[ARGS] DST: %s:%hu\n       SRC: %s:%hu\n", dst_ip, dst_port, src_ip, src_port);
+
+    init_graph();
+    init_surf(&surface);
 
     init_socket(&sd, &dst_addr, dst_ip, &dst_port);
     p_id = init_connect(sd, &dst_addr);
@@ -28,30 +32,21 @@ int main(int argc, char **argv) {
     printf("Connected to the server: %s:%hu (host name: %s)\n", inet_ntoa(dst_addr.sin_addr), ntohs(dst_addr.sin_port),
     ((dst_hst != NULL) ? dst_hst->h_name : ""));
 
-    printf("\n");
+    /*printf("\n");
     for (i = 0; i < MAP_H; ++i) {
         for(j = 0; j < MAP_W; ++j)
             printf("%2d", map[i][j]);
 
         printf("\n");
     }
-    printf("\n");
+    printf("\n");*/
 
-/*1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111*/
-        if (initscr() == NULL) {
-                perror("initscr");
-                exit(EXIT_FAILURE);
-        }
-
-        noecho();
-        curs_set(0);
-        keypad(stdscr, true);
-        cbreak();
-/*1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111*/
+    print_map(surface, p_id);
 
     pthread_create(&ctl_hndl_tid, NULL, control_hndl, (void *)&c_info);
     pthread_join(ctl_hndl_tid, (void **)status);
 
+    del_surf(&surface);
     close(sd);
     exit(EXIT_SUCCESS);
 }
