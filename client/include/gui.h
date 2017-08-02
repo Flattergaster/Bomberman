8ecf91ddaf8dd4ad8ca210e41eba31b62d261423
pro100gin <pro100gin100@gmail.com>
2017-08-02 09:58:37 +0600

    GUI

--- client/include/gui.h
+++ client/include/gui.h
@@ -1,11 +1,11 @@
 #define _GNU_SORCE
 
 #include <locale.h>
-#include <curses.h>
 #include <wchar.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncursesw/ncurses.h>
 /*#include "../../shared/share.h"*/
 /*#include "../../shared/include/utils.h"*/
 
@@ -34,9 +34,9 @@ void init_surf(surface_t **surface);
 #define P_MIN_ID     200
 #define P_MAX_ID     210
 
-#define PLAYER_U     L"00D6"
-#define ENEMY_U      L"00D8"
-#define BOMB_U       L"25CF"
-#define ST_CELL_U    L"25A6"
-#define BR_CELL_U    L"25A4"
-#define EMPTY_CELL_U L"0020"
+#define PLAYER_U     L"\u00D6"
+#define ENEMY_U      L"\u00D8"
+#define BOMB_U       L"\u25CF"
+#define ST_CELL_U    L"\u25A6"
+#define BR_CELL_U    L"\u25A4"
+#define EMPTY_CELL_U L"\u1234"
